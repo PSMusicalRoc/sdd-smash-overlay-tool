a082e69c117a56ae9c6e4042c1a020d312ccb368
Andrew <andrewchoi6534@gmail.com>
2024-07-10 15:40:09 -0400

    Update Functionality completed --Needs changes for relative path

--- src/MainWindow.cpp
+++ src/MainWindow.cpp
@@ -1,13 +1,25 @@
 #include "MainWindow.h"
+#include "updateFunctionality/nlohmann/json.hpp"
 
 #include "imgui.h"
 #include <iostream>
+#include <fstream>
 
 void MainWindow::render()
 {
     if (ImGui::Button("TestButton"))
     {
         std::cout << "Hello World" << std::endl;
+        using json = nlohmann::json;
+
+        std::ifstream f("/Users/choia8/SDND/EZStream/ez-stream-tool/src/overlay-data.json");
+        
+        json data = json::parse(f);
+        data["p1Name"] = "Tester";
+        std::ofstream out("/Users/choia8/SDND/EZStream/ez-stream-tool/src/overlay-data.json");
+        
+        out << std::setw(4) << data << std::endl;
+
     }
 
     ImGui::Text("A");

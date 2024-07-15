56f6e5633bf82a59092d2be1f0f894742bd8118d
DumbAsARoc <timbishop514@gmail.com>
2024-07-15 09:22:45 -0400

    added an abstract widget class for future widgets, should help with positioning and sizing

---
+++ include/Widget.h
@@ -0,0 +1,18 @@
+#ifndef EZSTREAM_WIDGET_INTERFACE
+#define EZSTREAM_WIDGET_INTERFACE
+
+class Widget
+{
+protected:
+    // supplied width and height of object
+    int _width, _height;
+    // supplied x and y position of the object
+    int _x, _y;
+
+    Widget(int x, int y, int width, int height);
+
+public:
+    virtual void render() = 0;
+};
+
+#endif
---
+++ src/Widget.cpp
@@ -0,0 +1,9 @@
+#include "Widget.h"
+
+Widget::Widget(int x, int y, int width, int height)
+{
+    this->_x = x;
+    this->_y = y;
+    this->_width = width;
+    this->_height = height;
+}

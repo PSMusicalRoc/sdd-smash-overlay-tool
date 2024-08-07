a1cd81f7f91c8880712c3a11aaee4a9bf97987f6
DumbAsARoc <timbishop514@gmail.com>
2024-08-07 15:38:59 -0400

    finished with render caching, should work fine but there could be bugs

--- src/PlayerCharacterSelect.cpp
+++ src/PlayerCharacterSelect.cpp
@@ -8,8 +8,35 @@
 #include "Update.h"
 #include "MainWindow.h"
 #include "ImageContainer.h"
+#include "CharacterCache.h"
 
 #include "WindowState.h"
+
+void switchCharacterRenders(const std::string& old_character, const std::string& new_character, bool is_player_one)
+{
+    Character* c = CharacterCache::get()->getCharacter(old_character);
+    if ((is_player_one && old_character != Update::get()->getData<std::string>("p2Character") && c != nullptr) ||
+        (!is_player_one && old_character != Update::get()->getData<std::string>("p1Character") && c != nullptr))
+    {
+        int costumes = c->getNumCostumes();
+        for (int i = 1; i <= costumes; i++)
+        {
+            ImageContainer::get()->unloadImage(ImageContainer::makeRenderImgKey(old_character, i));
+        }
+    }
+
+    c = CharacterCache::get()->getCharacter(new_character);
+    if ((is_player_one && new_character != Update::get()->getData<std::string>("p2Character") && c != nullptr) ||
+        (!is_player_one && new_character != Update::get()->getData<std::string>("p1Character") && c != nullptr))
+    {
+        int costumes = c->getNumCostumes();
+        for (int i = 1; i <= costumes; i++)
+        {
+            ImageContainer::get()->loadImage(ImageContainer::makeRenderImgKey(new_character, i), "res/characters/" + new_character
+                + "/render" + std::to_string(i) + ".png");
+        }
+    }
+}
 
 
 PlayerCharacterSelect::PlayerCharacterSelect(bool player1, int x, int y, int width, const std::string& label, const std::string& json_label)
@@ -19,12 +46,7 @@ PlayerCharacterSelect::PlayerCharacterSelect(bool player1, int x, int y, int wid
     this->_label = label;
     this->_json_label = json_label;
     this->_name = "default";
-
-    //SDL_Window* selectWindow = SDL_CreateWindow( "Player Select Screen",
-      //  SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
-        //1000, 500, SDL_WINDOW_SHOWN
-    //);
-     bool r = false;
+    this->_prev_character = "";
 }
 
 void PlayerCharacterSelect::render()
@@ -60,6 +82,12 @@ void PlayerCharacterSelect::render()
 
     if(_is_player_one) {
         cname = Update::get()->getData<std::string>("p1Character");
+        if (cname != _prev_character)
+        {
+            switchCharacterRenders(_prev_character, cname, true);
+            _prev_character = cname;
+        }
+
         costume = Update::get()->getData<int>("p1Skin");
         if(ImGui::ImageButton(("##" + _label).c_str(), ImageContainer::get()->getImage(ImageContainer::makeRenderImgKey(cname, costume)) ,ImVec2(_width, _width))) {
             Update::get()->set("SelectScreenOpen", "true");
@@ -67,6 +95,12 @@ void PlayerCharacterSelect::render()
         }
     } else {
         cname = Update::get()->getData<std::string>("p2Character");
+        if (cname != _prev_character)
+        {
+            switchCharacterRenders(_prev_character, cname, false);
+            _prev_character = cname;
+        }
+
         costume = Update::get()->getData<int>("p2Skin");
         if(ImGui::ImageButton(("##" + _label).c_str(), ImageContainer::get()->getImage(ImageContainer::makeRenderImgKey(cname, costume)) ,ImVec2(_width, _width))) {
             Update::get()->set("SelectScreenOpen", "true");
